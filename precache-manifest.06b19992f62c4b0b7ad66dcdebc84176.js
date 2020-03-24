8c0857012fddc5996a7447db07c4994c084f1ddf
DinhKienDev <you@example.com>
2020-03-24 23:30:09 +0700

    Updates

---
+++ precache-manifest.06b19992f62c4b0b7ad66dcdebc84176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d09b0c495ca03a1e21295727177f22",
+    "url": "/Project-TodoList-Redux/index.html"
+  },
+  {
+    "revision": "b8af1fe4607f697e248b",
+    "url": "/Project-TodoList-Redux/static/css/main.c0ea2230.chunk.css"
+  },
+  {
+    "revision": "36bd92259815411b978a",
+    "url": "/Project-TodoList-Redux/static/js/2.a735b378.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Project-TodoList-Redux/static/js/2.a735b378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8af1fe4607f697e248b",
+    "url": "/Project-TodoList-Redux/static/js/main.3b1bff92.chunk.js"
+  },
+  {
+    "revision": "2983ef1f2f73831c1442",
+    "url": "/Project-TodoList-Redux/static/js/runtime-main.15fc554b.js"
+  }
+]);
